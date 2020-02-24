b04b91ab6e37db7d668509d17df8705c58bbadeb
anisasyami <anisasyami.siswa@smkn4bdg.sch.id>
2020-02-24 15:20:18 +0700

    Commit kesatu

---
+++ hapus.php
@@ -0,0 +1,3 @@
+<?php
+	echo "ini adalah file hapus";
+	echo "Ini adalah file baru namanya hapus";  ?>
---
+++ tambah.php
@@ -0,0 +1,4 @@
+<?php
+echo "Ini adalah tambahan, udah gt aja bro";
+echo "Tambahan aja inimah"; 
+?>
